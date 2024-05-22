8b0a7bbf9f0a05d349624c11f51dedd1c4df9887
Adarsh Karan <adarshkaran01@gmail.com>
2024-05-22 11:59:01 +0100

    TASK1- made changes to simulation and navigation launch files

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -5,14 +5,14 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription, GroupAction
+from launch.actions import IncludeLaunchDescription, GroupAction, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node, PushRosNamespace
 from launch.conditions import IfCondition
 
-MY_NEO_ROBOT = os.environ.get('MY_ROBOT', "mpo_700")
-MY_NEO_ENVIRONMENT = os.environ.get('MAP_NAME', "neo_workshop")
+# MY_NEO_ROBOT = os.environ.get('MY_ROBOT', "mpo_700")
+# MY_NEO_ENVIRONMENT = os.environ.get('MAP_NAME', "neo_workshop")
 
 def generate_launch_description():
     use_multi_robots = LaunchConfiguration('use_multi_robots', default='False')
@@ -20,48 +20,86 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='True')
     namespace = LaunchConfiguration('namespace', default='')
     use_namespace = LaunchConfiguration('use_namespace', default='False')
-    map_dir = LaunchConfiguration(
-        'map',
-        default=os.path.join(
-            get_package_share_directory('neo_simulation2'),
-            'maps',
-            MY_NEO_ENVIRONMENT+'.yaml'))
 
-    param_file_name = 'navigation.yaml'
-    param_dir = LaunchConfiguration(
-        'params_file',
-        default=os.path.join(
-            get_package_share_directory('neo_simulation2'),
-            'configs/'+MY_NEO_ROBOT,
-            param_file_name))
+    # Declare launch arguments 'MY_ROBOT' and 'MAP_NAME' with default values and descriptions
+    declare_my_robot_arg = DeclareLaunchArgument(
+        'MY_ROBOT',
+        default_value='mpo_700',
+        description='Robot Types: "mpo_700", "mpo_500", "mp_400", "mp_500"') 
+       
+    declare_map_name_arg = DeclareLaunchArgument(
+        'MAP_NAME',
+        default_value='neo_workshop',
+        description='Map Types: "neo_track1", "neo_track2", "neo_workshop"')
+    
+    # Create launch configuration variables for the robot and map name
+    MY_NEO_ROBOT_ARG = LaunchConfiguration('MY_ROBOT')
+    MY_NEO_ENVIRONMENT_ARG = LaunchConfiguration('MAP_NAME')
+    
+    # OpaqueFunction is used to perform setup actions during launch through a Python function
+    def launch_setup(context, *args, **kwargs):
+        MY_NEO_ROBOT = MY_NEO_ROBOT_ARG.perform(context)
+        MY_NEO_ENVIRONMENT = MY_NEO_ENVIRONMENT_ARG.perform(context)
 
-    nav2_launch_file_dir = os.path.join(get_package_share_directory('neo_nav2_bringup'), 'launch')
+        map_dir = LaunchConfiguration(
+            'map',
+            default=os.path.join(
+                get_package_share_directory('neo_simulation2'),
+                'maps',
+                MY_NEO_ENVIRONMENT+'.yaml'))
+
+        param_file_name = 'navigation.yaml'
+        param_dir = LaunchConfiguration(
+            'params_file',
+            default=os.path.join(
+                get_package_share_directory('neo_simulation2'),
+                'configs/'+MY_NEO_ROBOT,
+                param_file_name))
+        
+        launch_actions = []
+        nav2_launch_file_dir = os.path.join(get_package_share_directory('neo_nav2_bringup'), 'launch')
+
+        # Include the localization_neo.launch.py file if use_amcl is not true
+        launch_actions.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([nav2_launch_file_dir, '/localization_neo.launch.py']),
+                condition=IfCondition(PythonExpression(['not ', use_amcl])),
+                launch_arguments={
+                    'map': map_dir,
+                    'use_sim_time': use_sim_time,
+                    'use_multi_robots': use_multi_robots,
+                    'params_file': param_dir,
+                    'namespace': namespace}.items(),
+            )
+        )
+
+        # Include the localization_amcl.launch.py file if use_amcl is true
+        launch_actions.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([nav2_launch_file_dir, '/localization_amcl.launch.py']),
+                condition=IfCondition(use_amcl),
+                launch_arguments={
+                    'map': map_dir,
+                    'use_sim_time': use_sim_time,
+                    'use_multi_robots': use_multi_robots,
+                    'params_file': param_dir,
+                    'namespace': namespace}.items(),
+            )
+        )
+
+        # Include the navigation_neo.launch.py file
+        launch_actions.append(
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([nav2_launch_file_dir, '/navigation_neo.launch.py']),
+                launch_arguments={'namespace': namespace,
+                                'use_sim_time': use_sim_time,
+                                'params_file': param_dir}.items()),
+        )
+
+        return launch_actions
 
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/localization_neo.launch.py']),
-            condition=IfCondition(PythonExpression(['not ', use_amcl])),
-            launch_arguments={
-                'map': map_dir,
-                'use_sim_time': use_sim_time,
-                'use_multi_robots': use_multi_robots,
-                'params_file': param_dir,
-                'namespace': namespace}.items(),
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/localization_amcl.launch.py']),
-            condition=IfCondition(use_amcl),
-            launch_arguments={
-                'map': map_dir,
-                'use_sim_time': use_sim_time,
-                'use_multi_robots': use_multi_robots,
-                'params_file': param_dir,
-                'namespace': namespace}.items(),
-        ),
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/navigation_neo.launch.py']),
-            launch_arguments={'namespace': namespace,
-                              'use_sim_time': use_sim_time,
-                              'params_file': param_dir}.items()),
+        declare_my_robot_arg,
+        declare_map_name_arg,
+        OpaqueFunction(function=launch_setup)
         ])
--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -4,57 +4,95 @@
 import launch
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration, PathJoinSubstitution, TextSubstitution, PythonExpression
 from launch_ros.actions import Node
 import os
 from pathlib import Path
 
-MY_NEO_ROBOT = os.environ.get('MY_ROBOT', "mpo_700")
-MY_NEO_ENVIRONMENT = os.environ.get('MAP_NAME', "neo_workshop")
 
 def generate_launch_description():
-    default_world_path = os.path.join(get_package_share_directory('neo_simulation2'), 'worlds', MY_NEO_ENVIRONMENT + '.world')
-
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-
-    robot_dir = LaunchConfiguration(
-        'robot_dir',
-        default=os.path.join(get_package_share_directory('neo_simulation2'),
-            'robots/'+MY_NEO_ROBOT,
-            MY_NEO_ROBOT+'.urdf'))
-
+    
+    # Declare launch arguments 'MY_ROBOT' and 'MAP_NAME' with default values and descriptions
+    declare_my_robot_arg = DeclareLaunchArgument(
+        'MY_ROBOT',
+        default_value='mpo_700',
+        description='Robot Types: "mpo_700", "mpo_500", "mp_400", "mp_500"') 
+       
+    declare_map_name_arg = DeclareLaunchArgument(
+        'MAP_NAME',
+        default_value='neo_workshop',
+        description='Map Types: "neo_track1", "neo_track2", "neo_workshop"')
+    
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='false',
+        default_value='True',
         description='Use simulation (Gazebo) clock if true')
-    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
-    urdf = os.path.join(get_package_share_directory('neo_simulation2'), 'robots/'+MY_NEO_ROBOT+'/', MY_NEO_ROBOT+'.urdf')
+    
+    declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
+        'use_robot_state_pub',
+        default_value='true',
+        description='Use robot state publisher if true')
+    
+    # Create launch configuration variables for the robot and map name
+    MY_NEO_ROBOT_ARG = LaunchConfiguration('MY_ROBOT')
+    MY_NEO_ENVIRONMENT_ARG = LaunchConfiguration('MAP_NAME')
 
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',arguments=['-entity', MY_NEO_ROBOT, '-file', urdf], output='screen')
+    USE_SIM_TIME_ARG = LaunchConfiguration('use_sim_time')
+    USE_ROBOT_STATE_PUB_ARG = LaunchConfiguration('use_robot_state_pub')
+    
+    # OpaqueFunction is used to perform setup actions during launch through a Python function
+    def launch_setup(context, *args, **kwargs):
 
-    start_robot_state_publisher_cmd = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        output='screen',
-        parameters=[{'use_sim_time': use_sim_time}],
-        arguments=[urdf])
+        # The perform method of a LaunchConfiguration is called to evaluate its value.
+        MY_NEO_ROBOT = MY_NEO_ROBOT_ARG.perform(context)
+        MY_NEO_ENVIRONMENT = MY_NEO_ENVIRONMENT_ARG.perform(context)
+        use_sim_time = USE_SIM_TIME_ARG.perform(context)
+        use_robot_state_pub = USE_ROBOT_STATE_PUB_ARG.perform(context)
 
-    teleop =  Node(package='teleop_twist_keyboard',executable="teleop_twist_keyboard",
-    output='screen',
-    prefix = 'xterm -e',
-    name='teleop')
+        default_world_path = os.path.join(get_package_share_directory('neo_simulation2'), 'worlds', MY_NEO_ENVIRONMENT + '.world')
 
-    gazebo = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
-            ),
-            launch_arguments={
-                'world': default_world_path,
-                'verbose': 'true',
-            }.items()
-        )
+        # robot_dir = LaunchConfiguration(
+        #     'robot_dir',
+        #     default=os.path.join(get_package_share_directory('neo_simulation2'),
+        #         'robots/'+MY_NEO_ROBOT,
+        #         MY_NEO_ROBOT+'.urdf'))
+        
+        gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+                ),
+                launch_arguments={
+                    'world': default_world_path,
+                    'verbose': 'true',
+                }.items()
+            )
+        urdf = os.path.join(get_package_share_directory('neo_simulation2'), 'robots/'+MY_NEO_ROBOT+'/', MY_NEO_ROBOT+'.urdf')
+        
+        spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',arguments=['-entity', MY_NEO_ROBOT, '-file', urdf], output='screen')
+        
+        # if use_robot_state_pub is true, start the robot state publisher node
+        if use_robot_state_pub:
+            start_robot_state_publisher_cmd = Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name='robot_state_publisher',
+                output='screen',
+                parameters=[{'use_sim_time': use_sim_time}],
+                arguments=[urdf])
 
-    return LaunchDescription([spawn_entity, start_robot_state_publisher_cmd, teleop, gazebo])
+        teleop =  Node(package='teleop_twist_keyboard',executable="teleop_twist_keyboard",
+            output='screen',
+            prefix = 'xterm -e',
+            name='teleop')
+
+        return [gazebo, spawn_entity, start_robot_state_publisher_cmd, teleop]
+
+    return LaunchDescription([
+        declare_my_robot_arg,
+        declare_map_name_arg,
+        declare_use_sim_time_cmd,
+        declare_use_robot_state_pub_cmd,
+        OpaqueFunction(function=launch_setup)
+        ])
