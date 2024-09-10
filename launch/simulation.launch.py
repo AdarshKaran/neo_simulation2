46bc36ada6d2c7fce9f81feac5e9e443335d7025
Adarsh Karan Kesavadas Prasanth <adarshkaran01@gmail.com>
2024-09-10 12:37:12 +0530

    Updated launch file description

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -16,16 +16,13 @@ import xacro
 """
 Description:
 
-This launch file is used to start a ROS2 simulation for a Neobotix robot in a specified environment. 
-It sets up the Gazebo simulator with the chosen robot and environment, 
-optionally starts the robot state publisher, and enables keyboard teleoperation.
+This file starts Gazebo simulator with the chosen Neobotix robot base, robot arm and simulation environment. 
 
-You can launch this file using the following terminal commands:
+The following command launches the simulation with sample arguments.
+    `ros2 launch neo_simulation2 simulation.launch.py my_robot:=mp_500 map_name:=neo_track1 use_sim_time:=true use_robot_state_pub:=true`
 
-1. `ros2 launch neo_simulation2 simulation.launch.py --show-args`
-   This command shows the arguments that can be passed to the launch file.
-2. `ros2 launch neo_simulation2 simulation.launch.py my_robot:=mp_500 map_name:=neo_track1 use_sim_time:=true use_robot_state_pub:=true`
-   This command launches the simulation with sample values for the arguments.
+To list the arguments that can be passed, use the following command.
+    `ros2 launch neo_simulation2 simulation.launch.py --show-args`
 """
 
 # OpaqueFunction is used to perform setup actions during launch through a Python function
@@ -37,13 +34,14 @@ def launch_setup(context: LaunchContext, my_neo_robot_arg, my_neo_env_arg, my_ne
     my_neo_environment = my_neo_env_arg.perform(context)
     my_neo_robot_arm = my_neo_robot_arm_arg.perform(context)
     use_sim_time = use_sim_time_arg.perform(context).lower() == 'true'
-
+    
     # Get the required paths for the world and robot robot_description_urdf
     default_world_path = os.path.join(get_package_share_directory('neo_simulation2'), 'worlds', my_neo_environment + '.world')
     robot_description_urdf = os.path.join(get_package_share_directory('neo_simulation2'), 'robots/'+my_neo_robot+'/', my_neo_robot+'.urdf.xacro')
+
     # use_gazebo is set to True since this code launches the robot in simulation
     xacro_args = {'use_gazebo': 'true','arm_type': my_neo_robot_arm, 'robot_prefix': my_neo_robot}
-    # Use xacro to process the file
+    # Use xacro to process the file with the argunments above
     robot_description_xacro = xacro.process_file(robot_description_urdf, mappings=xacro_args).toxml()
 
     gazebo = IncludeLaunchDescription(
@@ -80,11 +78,11 @@ def launch_setup(context: LaunchContext, my_neo_robot_arg, my_neo_env_arg, my_ne
         prefix = 'xterm -e',
         name='teleop'
         )
-    
+
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        arguments=["joint_state_broadcaster", "-c", "/controller_manager"],
         )
 
     initial_joint_controller_spawner_stopped = Node(
@@ -98,6 +96,7 @@ def launch_setup(context: LaunchContext, my_neo_robot_arg, my_neo_env_arg, my_ne
     if my_neo_robot_arm != '':
         launch_actions.append(joint_state_broadcaster_spawner)
         launch_actions.append(initial_joint_controller_spawner_stopped)
+
     launch_actions.append(gazebo)
     launch_actions.append(spawn_entity)
 
