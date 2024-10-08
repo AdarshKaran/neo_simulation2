a8f873130336cbf30aa058c6e537a0b9894c7e40
Adarsh Karan Kesavadas Prasanth <adarshkaran01@gmail.com>
2024-10-08 10:41:30 +0530

    converted all robots into modular xacros, added moveit support for ur5e and ur10 with mpo_500 and mpo_700, manually merged and resolved conflicts

--- launch/simulation.launch.py
+++ launch/simulation.launch.py
@@ -50,16 +50,16 @@ def launch_setup(context: LaunchContext, my_neo_robot_arg, my_neo_env_arg, robot
         # Incase of an invalid selection
         print("Invalid option, setting mpo_700 by default")
         my_neo_robot = "mpo_700"
-    
+
     with open('robot_name.txt', 'w') as file:
         file.write(my_neo_robot)
-    
+
     # Remove arm_type if robot does not support it
-    if (robot_arm_type != ""):
+    if (robot_arm_type != ''):
         if (my_neo_robot != "mpo_700" and my_neo_robot != "mpo_500"):
             print("Robot does not support arm, setting arm_type to empty")
-            robot_arm_type = ""
-    
+            robot_arm_type = ''
+
     # Get the required paths for the world and robot robot_description_urdf
     if (my_neo_environment == "neo_workshop" or my_neo_environment == "neo_track1"):
         world_path = os.path.join(
@@ -69,32 +69,6 @@ def launch_setup(context: LaunchContext, my_neo_robot_arg, my_neo_env_arg, robot
     else:
         world_path = my_neo_environment
 
-    if (my_neo_robot == "mpo_700" or my_neo_robot == "mpo_500"):
-        # Getting the robot description xacro files
-        robot_description_xacro = os.path.join(
-            get_package_share_directory('neo_simulation2'),
-            'robots/'+my_neo_robot+'/',
-            my_neo_robot+'.urdf.xacro')
-        
-        # use_gazebo is set to True since this code launches the robot in simulation
-        xacro_args = {
-            'use_gazebo': 'true',
-            'arm_type': robot_arm_type, 
-            'robot_prefix': my_neo_robot
-        }
-
-        # Use xacro to process the file with the argunments above
-        robot_description_file = xacro.process_file(
-            robot_description_xacro, 
-            mappings=xacro_args
-            ).toxml()
-    else:
-        # Getting the robot description urdf
-        robot_description_file = os.path.join(
-            get_package_share_directory('neo_simulation2'),
-            'robots/'+my_neo_robot+'/',
-            my_neo_robot+'.urdf')
-    
     # Setting the world and starting the Gazebo
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -106,11 +80,35 @@ def launch_setup(context: LaunchContext, my_neo_robot_arg, my_neo_env_arg, robot
         }.items()
     )
 
+    # Getting the robot description xacro
+    robot_description_xacro = os.path.join(
+        get_package_share_directory('neo_simulation2'),
+        'robots/'+my_neo_robot+'/',
+        my_neo_robot+'.urdf.xacro')
+    
+    # mpo robots have the option to mount arms
+    if (my_neo_robot == "mpo_700" or my_neo_robot == "mpo_500"):
+        # use_gazebo is set to True since this code launches the robot in simulation
+        xacro_args = {
+            'use_gazebo': 'true',
+            'arm_type': robot_arm_type, 
+        }
+    else:
+        xacro_args = {
+            'use_gazebo': 'true',
+        }
+
+    # Use xacro to process the file with the argunments above
+    robot_description_file = xacro.process_file(
+        robot_description_xacro, 
+        mappings=xacro_args
+        ).toxml()
+
     # Spawning the robot
     spawn_entity = Node(
         package='gazebo_ros', 
         executable='spawn_entity.py',
-        arguments=['-entity', my_neo_robot,'-file', robot_description_file], 
+        arguments=['-entity', my_neo_robot,'-topic', '/robot_description'], 
         output='screen'
     )
 
@@ -120,8 +118,8 @@ def launch_setup(context: LaunchContext, my_neo_robot_arg, my_neo_env_arg, robot
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        parameters=[{'use_sim_time': use_sim_time}],
-        arguments=[robot_description_file]
+        parameters=[{'use_sim_time': use_sim_time, 
+                    'robot_description': robot_description_file}]
     )
 
     # Starting the teleop node
@@ -144,7 +142,7 @@ def launch_setup(context: LaunchContext, my_neo_robot_arg, my_neo_env_arg, robot
         executable="spawner",
         arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
     )
-    
+
     # See Issue: https://github.com/ros2/rclpy/issues/1287
     # Cannot delete the newly create file. The user has to delete it on his own
     # Refer documentation for more info
@@ -189,7 +187,7 @@ def generate_launch_description():
         '\t Elite Arms: ec66, cs66\n'
         '\t Universal Robotics: ur5, ur10, ur5e, ur10e'        
     )
-    
+
     # Create launch configuration variables for the robot and map name
     my_neo_robot_arg = LaunchConfiguration('my_robot')
     my_neo_env_arg = LaunchConfiguration('world')
@@ -209,3 +207,4 @@ def generate_launch_description():
     ld.add_action(opq_function)
 
     return ld
+
